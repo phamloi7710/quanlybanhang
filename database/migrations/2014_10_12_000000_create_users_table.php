0d99357143c42bb90ca41d3bd76bf86baca8eda4
Phạm Lợi <phamloi7710@gmail.com>
2018-12-24 22:39:07 +0700

    Add Assets

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,36 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('is_admin')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('lang_code')->nullable();
+            $table->enum('status', array('active', 'inActive'))->default('inActive');
+            $table->rememberToken();
+            $table->timestamps();
+
+
+
+
+
+
+            $table->increments('id');
+            $table->string('username','64')->nullable()->unique();
+            $table->string('email','100')->unique();
+            $table->string('password','64');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('master_password','64')->nullable();
+            $table->tinyInteger('group_id')->default(0);
+            $table->tinyInteger('is_root')->default(0);
+            $table->string('isAdmin')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('lang_id')->nullable();
+            $table->integer('time_expire')->nullable();
+            $table->tinyInteger('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
